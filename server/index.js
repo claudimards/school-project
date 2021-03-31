394fd828006acc746ff975aab096aa22e6d90d4e
Claudimar Bezerra <claudimards@gmail.com>
2021-03-31 10:21:42 -0300

    Initial commit, project structure setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json({ limit: '20mb', extended: true }))
+app.use(express.urlencoded({limit: '20mb', extended: true}))
+app.use(cors())
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port --> ${PORT}`))
+}).catch((error) => console.log(`Something went wrong! Error.: ${error}`))
+
+mongoose.set('useFindAndModify', false)
